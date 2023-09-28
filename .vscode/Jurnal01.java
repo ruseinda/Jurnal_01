56608e11058d3ddc4f9445ffc4d1824234aa2ca0
Alief Akbar Putra Ruseinda <ruseindaalief@domain.com>
2023-09-28 16:22:12 +0700

    Jurnal01 Commit

--- .vscode/Jurnal01.java
+++ .vscode/Jurnal01.java
@@ -2,5 +2,9 @@ public class Jurnal01 {
 
     public static void main(String[] args) {
 
+        System.out.println("Nama:" + " ALief AKbar Putra Ruseinda");
+        System.out.println("NIM:" + " 6706220035");
+        System.out.println("Kelas:" + " D3IF-46-02");
+
     }
 }
